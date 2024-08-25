701da2c0d049ed9b525d017a2f58c5a9386e0d9d
gwjang <zmdlgl@gmail.com>
2024-08-25 19:45:08 +0900

    feat: 메시지 길이 제한

--- src/main/kotlin/makestar/makestarchat/presentation/ChatController.kt
+++ src/main/kotlin/makestar/makestarchat/presentation/ChatController.kt
@@ -11,10 +11,17 @@ import org.springframework.stereotype.Controller
 class ChatController(
     private val redisPublisher: RedisPublisher
 ) {
-
+    private val maxMessageLength = 1000
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     fun sendMessage(@Payload chatMessage: ChatMessage) {
+        validateMessage(chatMessage)
         redisPublisher.publish(chatMessage)
+    }
+
+    private fun validateMessage(chatMessage: ChatMessage) {
+        if (chatMessage.content.length > maxMessageLength) {
+            throw IllegalArgumentException("Message is too long")
+        }
     }
 }
